7d52a5e0f5c89bd4a0fffc0fa85e22b98398015f
Vo Minh Don <vominhdona1@gmail.com>
2021-10-07 09:45:05 +0700

    feat: create floating button

--- Exercise/Controller/EnvelopePageViewController.swift
+++ Exercise/Controller/EnvelopePageViewController.swift
@@ -13,20 +13,45 @@ class EnvelopePageViewController: UIPageViewController{
     let initialView = 0
     var rightBarButton = UIBarButtonItem()
     
+    private var leftFloatingButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 70, height: 70))
+        button.setBackgroundImage(UIImage(named: "sample_btn"), for: .normal)
+        button.layer.cornerRadius = 10
+        button.layer.shadowOpacity = 0.3
+        button.layer.shadowRadius = 7
+        return button
+    }()
+    
+    private var rightFloatingButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 70, height: 70))
+        button.setBackgroundImage(UIImage(named: "enlargement_btn"), for: .normal)
+        button.layer.cornerRadius = 10
+        button.layer.shadowOpacity = 0.2
+        button.layer.shadowRadius = 7
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        createLeftBarButton()
         setup()
         
         // Do any additional setup after loading the view.
     }
-    @IBAction func previousBarButtonItemPressed(_ sender: UIBarButtonItem) {
-        goToPreviousPage()
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        leftFloatingButton.frame = CGRect(x: 20, y: view.frame.size.height - 200, width: 70, height: 70)
+        rightFloatingButton.frame = CGRect(x: view.frame.size.width - 20 - 70, y: view.frame.size.height - 200, width: 70, height: 70)
     }
+    
     
     func setup() {
         delegate = self
         dataSource = self
+        view.addSubview(leftFloatingButton)
+        view.addSubview(rightFloatingButton)
+        initializeRightBarButton()
+        navigationItem.rightBarButtonItem = rightBarButton
         let firstView = FirstViewController(nibName: "FirstViewController", bundle: nil)
         let secondView = FirstViewController(nibName: "SecondViewController", bundle: nil)
         let thirdView = FirstViewController(nibName: "ThirdViewController", bundle: nil)
@@ -39,7 +64,7 @@ class EnvelopePageViewController: UIPageViewController{
         setViewControllers([pages[initialView]], direction: .forward, animated: true, completion: nil)
     }
     
-    func createLeftBarButton() {
+    func initializeRightBarButton() {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 35))
         button.setTitle("次へ", for: .normal)
         button.titleLabel?.font = .systemFont(ofSize: 14)
@@ -47,7 +72,10 @@ class EnvelopePageViewController: UIPageViewController{
         button.layer.cornerRadius = 5.0
         button.addTarget(self, action: #selector(didTapRightBarButton), for: .touchUpInside)
         rightBarButton = UIBarButtonItem(customView: button)
-        self.navigationItem.rightBarButtonItem = rightBarButton
+    }
+    
+    @IBAction func previousBarButtonItemPressed(_ sender: UIBarButtonItem) {
+        goToPreviousPage()
     }
     
     @objc func didTapRightBarButton(sender: UIButton!) {
@@ -82,7 +110,28 @@ extension EnvelopePageViewController: UIPageViewControllerDataSource {
 
 extension EnvelopePageViewController: UIPageViewControllerDelegate {
     func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
-        
+        guard let viewControllers = pageViewController.viewControllers else { return }
+        guard let currentIndex = pages.firstIndex(of: viewControllers[0]) else { return }
+        hideButtonsIfNeeded(with: currentIndex)
+    }
+    
+    private func hideButtonsIfNeeded(with index: Int)  {
+        let isLastPage = index == pages.count - 1
+        if isLastPage {
+            hideButtons()
+        } else {
+            showButtons()
+        }
+    }
+    
+    private func hideButtons() {
+        leftFloatingButton.isHidden = true
+        rightFloatingButton.isHidden = true
+    }
+    
+    private func showButtons() {
+        leftFloatingButton.isHidden = false
+        rightFloatingButton.isHidden = false
     }
 }
 
