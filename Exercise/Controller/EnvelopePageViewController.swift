e14ec254ad81e01bf00e3fa78bb008a44ac8032c
Vo Minh Don <vominhdona1@gmail.com>
2021-10-10 15:45:35 +0700

    refactor: group screen file, mark, constants

--- Exercise/Controller/EnvelopePageViewController.swift
+++ Exercise/Controller/EnvelopePageViewController.swift
@@ -10,35 +10,9 @@ import UIKit
 class EnvelopePageViewController: UIPageViewController{
     
     var pages = [UIViewController]()
-    let navigationTitle = ["バリエーションを選択", "配置したい写真を選択", "デザイン・差出人の編集", "デザインを最終確認"]
     let initialView = 0
-    //var rightBarButton = UIBarButtonItem()
+    let navigationTitle = Constants.navigationTitle
     @IBOutlet weak var rightBarButton: UIBarButtonItem!
-    
-//    private var leftFloatingButton: UIButton = {
-//        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 70, height: 70))
-//        button.setBackgroundImage(UIImage(named: "sample_btn"), for: .normal)
-//        button.layer.cornerRadius = 10
-//        button.layer.shadowOpacity = 0.3
-//        button.layer.shadowRadius = 7
-//        return button
-//    }()
-//
-//    private var rightFloatingButton: UIButton = {
-//        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 70, height: 70))
-//        button.setBackgroundImage(UIImage(named: "enlargement_btn"), for: .normal)
-//        button.layer.cornerRadius = 10
-//        button.layer.shadowOpacity = 0.2
-//        button.layer.shadowRadius = 7
-//        return button
-//    }()
-//
-//    private var helpFloatingButton: UIButton = {
-//        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
-//        button.setBackgroundImage(UIImage(named: "help"), for: .normal)
-//        button.layer.cornerRadius = 10
-//        return button
-//    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,29 +21,15 @@ class EnvelopePageViewController: UIPageViewController{
         // Do any additional setup after loading the view.
     }
     
-//    override func viewDidLayoutSubviews() {
-//        super.viewDidLayoutSubviews()
-//        leftFloatingButton.frame = CGRect(x: 20, y: view.frame.size.height - 200, width: 70, height: 70)
-//
-//        rightFloatingButton.frame = CGRect(x: view.frame.size.width - 20 - 70, y: view.frame.size.height - 200, width: 70, height: 70)
-//
-//        helpFloatingButton.frame = CGRect(x: view.frame.size.width - 20 - 30, y: 120, width: 30, height: 30)
-//    }
-    
-    
+    //MARK: - Set up
     func setup() {
         delegate = self
         dataSource = self
-//        view.addSubview(leftFloatingButton)
-//        view.addSubview(rightFloatingButton)
-//        view.addSubview(helpFloatingButton)
         initializeRightBarButton()
-        //navigationItem.rightBarButtonItem = rightBarButton
-        let firstView = FirstViewController(nibName: "FirstViewController", bundle: nil)
-        let secondView = FirstViewController(nibName: "SecondViewController", bundle: nil)
-        let thirdView = FirstViewController(nibName: "ThirdViewController", bundle: nil)
-        let fourView = FirstViewController(nibName: "FourViewController", bundle: nil)
-        
+        let firstView = FirstViewController(nibName: Constants.firstScreenIdentifier, bundle: nil)
+        let secondView = FirstViewController(nibName: Constants.secondScreenIdentifier, bundle: nil)
+        let thirdView = FirstViewController(nibName: Constants.thirdScreenIdentifier, bundle: nil)
+        let fourView = FirstViewController(nibName: Constants.fourScreenIdentifier, bundle: nil)
         pages.append(firstView)
         pages.append(secondView)
         pages.append(thirdView)
@@ -79,16 +39,17 @@ class EnvelopePageViewController: UIPageViewController{
     
     func initializeRightBarButton() {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 52, height: 35))
-        button.setTitle("次へ", for: .normal)
+        button.setTitle(Constants.rightBarButtonTitle, for: .normal)
         button.titleLabel?.font = .systemFont(ofSize: 11, weight: .bold)
         button.titleLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping
         button.titleLabel?.textAlignment = .center
-        button.backgroundColor = UIColor(named: "orange")
+        button.backgroundColor = UIColor(named: Constants.Color.orange)
         button.layer.cornerRadius = 5.0
         button.addTarget(self, action: #selector(didTapRightBarButton), for: .touchUpInside)
         rightBarButton.customView = button
     }
     
+    //MARK: - Page navigation
     @IBAction func previousBarButtonItemPressed(_ sender: UIBarButtonItem) {
         goToPreviousPage()
     }
@@ -99,14 +60,15 @@ class EnvelopePageViewController: UIPageViewController{
     
 }
 
+//MARK: - UIpageViewControllerDataSource
 extension EnvelopePageViewController: UIPageViewControllerDataSource {
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         guard let currentIndex = pages.firstIndex(of: viewController) else { return nil }
         
         if currentIndex == 0 {
-            return nil             // wrap to last
+            return nil
         } else {
-            return pages[currentIndex - 1]  // go previous
+            return pages[currentIndex - 1]
         }
     }
     
@@ -115,53 +77,34 @@ extension EnvelopePageViewController: UIPageViewControllerDataSource {
         guard let currentIndex = pages.firstIndex(of: viewController) else { return nil }
         
         if currentIndex < pages.count - 1 {
-            return pages[currentIndex + 1]  // go next
+            return pages[currentIndex + 1]
         } else {
-            return nil              // wrap to first
+            return nil
         }
     }
 }
 
-
+//MARK: - UIPageViewControllerDelegate
 extension EnvelopePageViewController: UIPageViewControllerDelegate {
     func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
         guard let viewControllers = pageViewController.viewControllers else { return }
         guard let currentIndex = pages.firstIndex(of: viewControllers[0]) else { return }
-        //hideButtonsIfNeeded(with: currentIndex)
         changeNavigationItem(with: currentIndex)
     }
-    
-//    private func hideButtonsIfNeeded(with index: Int)  {
-//        let isLastPage = index == pages.count - 1
-//        if isLastPage {
-//            hideButtons()
-//        } else {
-//            showButtons()
-//        }
-//    }
     
     private func changeNavigationItem(with index: Int)  {
         let isLastPage = index == pages.count - 1
         navigationItem.title = navigationTitle[index]
         let button = rightBarButton.customView as! UIButton
         if isLastPage {
-            button.setTitle("注文へ\n進む", for: .normal)
+            button.setTitle(Constants.rightBarButtonEndTitle, for: .normal)
         } else {
-            button.setTitle("次へ", for: .normal)
+            button.setTitle(Constants.rightBarButtonTitle, for: .normal)
         }
     }
-    
-//    private func hideButtons() {
-//        leftFloatingButton.isHidden = true
-//        rightFloatingButton.isHidden = true
-//    }
-//
-//    private func showButtons() {
-//        leftFloatingButton.isHidden = false
-//        rightFloatingButton.isHidden = false
-//    }
 }
 
+//MARK: - UIPageViewController Navigation
 extension UIPageViewController {
     func goToNextPage(animated: Bool = true, completion: ((Bool) -> Void)? = nil) {
         guard let currentPage = viewControllers?[0] else { return }
